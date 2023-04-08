fa661853f27018b5c0e02fffe9861542c16ca865
NaylaDeLis <gfirmansyah279@gmail.com>
2023-04-08 14:53:57 +0700

    DB in Go v.1.0.1

--- main.go
+++ main.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/NaylaDeLis/Go-6-Database/services"
-)
-
 func main() {
-	db := services.GetConnection()
-	defer db.Close()
-
-	ctx := context.Background()
-
-	_, err := db.ExecContext(ctx, "INSERT INTO Customer(id,name) VALUES ('Eko', eko)")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Success inserting to Customer")
 }
